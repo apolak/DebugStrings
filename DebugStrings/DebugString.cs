1e4f3a26f2de8a58505957b9d919ab4336a83ad4
Aleksander Polak <apolak@gmail.com>
2013-07-27 14:06:09 +0200

    Implement synchronous debug string monitoring

--- DebugStrings/DebugString.cs
+++ DebugStrings/DebugString.cs
@@ -1,28 +1,126 @@
 ﻿namespace DebugStrings
 {
     using System;
+    using System.ComponentModel;
+    using System.Diagnostics;
     using System.Runtime.InteropServices;
     using System.Security;
 
     /// <summary>
-    /// Provides static methods for writing values to the debug output.
+    /// Defines the unique identifier of the process that has written data to the debug output
+    /// and the text representation of the object that has been written.
     /// </summary>
-    public static class DebugString
+    [Serializable]
+    [DebuggerDisplay("{DebuggerDisplay,nq}")]
+    public struct DebugString : IEquatable<DebugString>
     {
         /// <summary>
-        /// The value indicating whether operations performed by the <see cref="DebugString"/> class are supported
-        /// on the current platform.
+        /// The value indicating whether operations performed by the <see cref="DebugString"/> class
+        /// are supported on the current platform.
         /// </summary>
         public static readonly bool IsPlatformSupported = CheckIsPlatformSupported();
 
         /// <summary>
-        /// Sends a string to the debugger for display.
+        /// The unique identifier of the process that has written data to the debug output.
         /// </summary>
-        /// <param name="lpOutputString">
-        /// The null-terminated string to be displayed.
+        private readonly int processId;
+
+        /// <summary>
+        /// The text representation of the object that has been written to the debug output.
+        /// </summary>
+        private readonly string value;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DebugString"/> struct with
+        /// the specified unique identifier of the process and the string value.
+        /// </summary>
+        /// <param name="processId">
+        /// The unique identifier of the process that has written the data.
         /// </param>
-        [DllImport("Kernel32.dll", CharSet = CharSet.Auto, BestFitMapping = false, ThrowOnUnmappableChar = true)]
-        private extern static void OutputDebugString(string lpOutputString);
+        /// <param name="value">
+        /// The text representation of the written object.
+        /// </param>
+        public DebugString(int processId, string value)
+        {
+            this.processId = processId;
+            this.value = value;
+        }
+
+        /// <summary>
+        /// Gets the unique identifier of the process that has written the data
+        /// to the debug output.
+        /// </summary>
+        public int ProcessId
+        {
+            get { return this.processId; }
+        }
+
+        /// <summary>
+        /// Gets the text representation of the object written to the debug output.
+        /// </summary>
+        public string Value
+        {
+            get { return this.value; }
+        }
+
+        /// <summary>
+        /// Gets the text representation of this <see cref="DebugData"/> instance for displaying
+        /// in the debugger.
+        /// </summary>
+        private string DebuggerDisplay
+        {
+            get { return string.Format("{{ProcessId:{0}, Value:\"{1}\"}", this.processId, this.value); }
+        }
+
+        /// <summary>
+        /// Converts the <see cref="DebugString"/> to a string.
+        /// </summary>
+        /// <param name="debugString">
+        /// The <see cref="DebugString"/> to convert.
+        /// </param>
+        /// <returns>
+        /// The string constructed from <paramref cref="value" />.
+        /// </returns>
+        public static explicit operator string(DebugString debugString)
+        {
+            return debugString.value;
+        }
+
+        /// <summary>
+        /// Determines whether two <see cref="DebugString"/>s are equal.
+        /// </summary>
+        /// <param name="left">
+        /// The first value to compare.
+        /// </param>
+        /// <param name="right">
+        /// The second value to compare.
+        /// </param>
+        /// <returns>
+        /// <c>true</c> if <paramref name="left"/> is equal to <paramref name="right"/>; otherwise,
+        /// <c>false</c>.
+        /// </returns>
+        public static bool operator ==(DebugString left, DebugString right)
+        {
+            return left.Equals(right);
+        }
+
+        /// <summary>
+        /// Determines whether two <see cref="DebugString"/>s are different.
+        /// </summary>
+        /// <param name="left">
+        /// The first value to compare.
+        /// </param>
+        /// <param name="right">
+        /// The second value to compare.
+        /// </param>
+        /// <returns>
+        /// <c>true</c> if <paramref name="left"/> is different from <paramref name="right"/>;
+        /// otherwise, <c>false</c>.
+        /// </returns>
+        public static bool operator !=(DebugString left, DebugString right)
+        {
+            return !left.Equals(right);
+        }
 
         /// <summary>
         /// Writes the specified string value to the debug output.
@@ -206,6 +304,70 @@
             }
 
             return true;
+        }
+
+        /// <summary>
+        /// Sends a string to the debugger for display.
+        /// </summary>
+        /// <param name="lpOutputString">
+        /// The null-terminated string to be displayed.
+        /// </param>
+        [DllImport("Kernel32.dll", CharSet = CharSet.Auto, BestFitMapping = false, ThrowOnUnmappableChar = true)]
+        private extern static void OutputDebugString(string lpOutputString);
+
+        /// <summary>
+        /// Determines whether this <see cref="DebugString" /> is equal to the specified
+        /// <see cref="DebugString" />.
+        /// </summary>
+        /// <param name="obj">
+        /// The <see cref="DebugString" /> to compare with this <see cref="DebugString" />.
+        /// </param>
+        /// <returns>
+        /// <c>true</c> if this <see cref="DebugString"/> is equal to <paramref name="obj" />;
+        /// otherwise, false.
+        /// </returns>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        public override bool Equals(object obj)
+        {
+            return (obj is DebugString) && this.Equals((DebugString)obj);
+        }
+
+        /// <summary>
+        /// Determines whether this <see cref="DebugString" /> is equal to the specified
+        /// <see cref="DebugString" />.
+        /// </summary>
+        /// <param name="other">
+        /// The <see cref="DebugString" /> to compare with this <see cref="DebugString" />.
+        /// </param>
+        /// <returns>
+        /// <c>true</c> if this <see cref="DebugString"/> is equal to <paramref name="other" />;
+        /// otherwise, false.
+        /// </returns>
+        public bool Equals(DebugString other)
+        {
+            return (this.processId == other.processId) && string.Equals(this.value, other.value);
+        }
+
+        /// <summary>
+        /// Gets the hash code for this <see cref="DebugString"/>.
+        /// </summary>
+        /// <returns>
+        /// The hash code for this <see cref="DebugString"/>.
+        /// </returns>
+        public override int GetHashCode()
+        {
+            return (this.value != null) ? this.processId ^ this.value.GetHashCode() : this.processId;
+        }
+
+        /// <summary>
+        /// Gets the text representation of the object that has been written to the debug output.
+        /// </summary>
+        /// <returns>
+        /// The text representation of the written object.
+        /// </returns>
+        public override string ToString()
+        {
+            return "[" + this.processId + "] " + this.value;
         }
     }
 }
---
+++ DebugStrings/DebugView.cs
@@ -0,0 +1,475 @@
+﻿namespace DebugStrings
+{
+    using System;
+    using System.Diagnostics;
+    using System.IO.MemoryMappedFiles;
+    using System.Security;
+    using System.Security.AccessControl;
+    using System.Text;
+    using System.Threading;
+
+    /// <summary>
+    /// Monitors values written to the debug output.
+    /// </summary>
+    public sealed class DebugView : IDisposable
+    {
+        /// <summary>
+        /// The length of the data buffer.
+        /// </summary>
+        private const int DataBufferLength = 4096;
+
+        /// <summary>
+        /// The <see cref="DebugView"/> that has not been associated with the debug output objects.
+        /// </summary>
+        private static readonly DebugView Detached = new DebugView();
+
+        /// <summary>
+        /// Indicates whether this <see cref="DebugView"/> has been successfully associated
+        /// with the debug output objects.
+        /// </summary>
+        private readonly bool attached;
+
+        /// <summary>
+        /// The memory-mapped file to which the data is written to.
+        /// </summary>
+        private MemoryMappedFile dataBufferFile;
+
+        /// <summary>
+        /// The event that this <see cref="DebugView"/> sets to signaled when the memory-mapped file
+        /// is ready to receive data.
+        /// </summary>
+        private EventWaitHandle bufferReadyEvent;
+
+        /// <summary>
+        /// The event that becomes signaled when data has been written to the memory-mapped file.
+        /// </summary>
+        private WaitHandle dataReadyEvent;
+
+        /// <summary>
+        /// The memory buffer to which the data is read from the memory-mapped file.
+        /// </summary>
+        private byte[] dataBuffer;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DebugView"/> class.
+        /// </summary>
+        private DebugView()
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DebugView"/> class.
+        /// </summary>
+        /// <param name="dataBufferFile">
+        /// The memory-mapped file to which the data is written to.
+        /// </param>
+        /// <param name="bufferReadyEvent">
+        /// The event that this <see cref="DebugView"/> sets to signaled when the memory-mapped file
+        /// is ready to receive data.
+        /// </param>
+        /// <param name="dataReadyEvent">
+        /// The event that becomes signaled when data has been written to the memory-mapped file.
+        /// </param>
+        private DebugView(
+            MemoryMappedFile dataBufferFile,
+            EventWaitHandle bufferReadyEvent,
+            WaitHandle dataReadyEvent)
+        {
+            Debug.Assert(dataBufferFile != null, "dataBufferFile is null");
+            Debug.Assert(bufferReadyEvent != null, "bufferReadyEvent is null");
+            Debug.Assert(dataReadyEvent != null, "dataReadyEvent is null");
+
+            this.attached = true;
+            this.dataBufferFile = dataBufferFile;
+            this.bufferReadyEvent = bufferReadyEvent;
+            this.dataReadyEvent = dataReadyEvent;
+            this.dataBuffer = new byte[DataBufferLength];
+
+            this.bufferReadyEvent.Set();
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether this <see cref="DebugView"/> has been successfully
+        /// associated with the debug output objects.
+        /// </summary>
+        public bool IsAttached
+        {
+            get { return this.attached; }
+        }
+
+        /// <summary>
+        /// Creates a new <see cref="DebugView"/>.
+        /// </summary>
+        /// <returns>
+        /// The newly created <see cref="DebugView"/>.
+        /// </returns>
+        public static DebugView CreateView()
+        {
+            const string MemoryMappedFileName = @"DBWIN_BUFFER";
+            const string BufferReadyEventName = @"DBWIN_BUFFER_READY";
+            const string DataReadyEventName = @"DBWIN_DATA_READY";
+
+            MemoryMappedFile dataBufferFile = null;
+            EventWaitHandle bufferReadyEvent = null;
+            WaitHandle dataReadyEvent = null;
+            bool createdNew = false;
+
+            try
+            {
+                dataBufferFile = MemoryMappedFile.CreateNew(
+                    MemoryMappedFileName,
+                    DataBufferLength,
+                    MemoryMappedFileAccess.ReadWrite);
+
+                bufferReadyEvent = new EventWaitHandle(
+                    false,
+                    EventResetMode.AutoReset,
+                    BufferReadyEventName,
+                    out createdNew);
+
+                if (!createdNew)
+                {
+                    return Detached;
+                }
+
+                dataReadyEvent = new EventWaitHandle(
+                    false,
+                    EventResetMode.AutoReset,
+                    DataReadyEventName,
+                    out createdNew);
+
+                if (!createdNew)
+                {
+                    return Detached;
+                }
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return Detached;
+            }
+            finally
+            {
+                if (!createdNew)
+                {
+                    if (dataBufferFile != null)
+                    {
+                        dataBufferFile.Dispose();
+                    }
+
+                    if (bufferReadyEvent != null)
+                    {
+                        bufferReadyEvent.Dispose();
+                    }
+
+                    if (dataReadyEvent != null)
+                    {
+                        dataReadyEvent.Dispose();
+                    }
+                }
+            }
+
+            var buffer = new DebugView(dataBufferFile, bufferReadyEvent, dataReadyEvent);
+            return buffer;
+        }
+        
+        /// <summary>
+        /// Removes a <see cref="DebugString"/> from this <see cref="DebugView"/>.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="DebugString"/> removed from this <see cref="DebugView"/>.
+        /// </returns>
+        [SecuritySafeCritical]
+        public DebugString Take()
+        {
+            return this.Take(CancellationToken.None);
+        }
+
+        /// <summary>
+        /// Removes a <see cref="DebugString"/> from the buffer contained by this
+        /// <see cref="DebugView"/>.
+        /// </summary>
+        /// <param name="cancellationToken">
+        /// The <see cref="CancellationToken"/> that can be used to cancel the take operation.
+        /// </param>
+        /// <returns>
+        /// The <see cref="DebugString"/> removed from this <see cref="DebugView"/>.
+        /// </returns>
+        [SecuritySafeCritical]
+        public DebugString Take(CancellationToken cancellationToken)
+        {
+            DebugString value;
+            bool taken = TryTake(out value, Timeout.Infinite, cancellationToken);
+            Debug.Assert(taken, "taken is false");
+
+            return value;
+        }
+
+        /// <summary>
+        /// Attempts to remove a <see cref="DebugString" /> from the buffer contained by this
+        /// <see cref="DebugView"/>.
+        /// </summary>
+        /// <param name="value">
+        /// When <see cref="TryTake(DebugString)"/> returns, contains the removed <see cref="DebugString"/>,
+        /// if successful; otherwise, an empty <see cref="DebugString"/>.
+        /// </param>
+        /// <returns>
+        /// <c>true</c> if the <see cref="DebugString"/> has been removed; otherwise, <c>false</c>.
+        /// </returns>
+        [SecuritySafeCritical]
+        public bool TryTake(out DebugString value)
+        {
+            return this.TryTake(out value, 0, CancellationToken.None);
+        }
+
+        /// <summary>
+        /// Attempts to remove a <see cref="DebugString" /> from the buffer contained by this
+        /// <see cref="DebugView"/> within the specified time.
+        /// </summary>
+        /// <param name="value">
+        /// When <see cref="TryTake(DebugString,int)"/> returns, contains the removed
+        /// <see cref="DebugString"/>, if successful; otherwise, an empty <see cref="DebugString"/>.
+        /// </param>
+        /// <param name="timeout">
+        /// The number of milliseconds to wait, or <see cref="Timeout.Infinite"/> (-1) to wait
+        /// indefinitely.
+        /// </param>
+        /// <returns>
+        /// <c>true</c> if the <see cref="DebugString"/> has been removed within the specified time;
+        /// otherwise, <c>false</c>.
+        /// </returns>
+        [SecuritySafeCritical]
+        public bool TryTake(out DebugString value, int timeout)
+        {
+            return this.TryTake(out value, timeout, CancellationToken.None);
+        }
+
+        /// <summary>
+        /// Attempts to remove a <see cref="DebugString" /> from the buffer contained by this
+        /// <see cref="DebugView"/> within the specified time.
+        /// </summary>
+        /// <param name="value">
+        /// When <see cref="TryTake(DebugString,TimeSpan)"/> returns, contains the removed
+        /// <see cref="DebugString"/>, if successful; otherwise, an empty <see cref="DebugString"/>.
+        /// </param>
+        /// <param name="timeout">
+        /// The <see cref="TimeSpan"/> that represents the number of milliseconds to wait,
+        /// or the <see cref="TimeSpan"/> that represents -1 milliseconds to wait indefinitely.
+        /// </param>
+        /// <returns>
+        /// <c>true</c> if the <see cref="DebugString"/> has been removed within the specified time;
+        /// otherwise, <c>false</c>.
+        /// </returns>
+        [SecuritySafeCritical]
+        public bool TryTake(out DebugString value, TimeSpan timeout)
+        {
+            return this.TryTake(out value, timeout, CancellationToken.None);
+        }
+
+        /// <summary>
+        /// Attempts to remove a <see cref="DebugString" /> from the buffer contained by this
+        /// <see cref="DebugView"/> within the specified time while observing a cancellation token.
+        /// </summary>
+        /// <param name="value">
+        /// When <see cref="TryTake(DebugString,TimeSpan,CancellationToken)"/> returns, contains
+        /// the removed <see cref="DebugString"/>, if successful; otherwise, an empty
+        /// <see cref="DebugString"/>.
+        /// </param>
+        /// <param name="timeout">
+        /// The <see cref="TimeSpan"/> that represents the number of milliseconds to wait,
+        /// or the <see cref="TimeSpan"/> that represents -1 milliseconds to wait indefinitely.
+        /// </param>
+        /// <param name="cancellationToken">
+        /// The cancellation token to observe.
+        /// </param>
+        /// <returns>
+        /// <c>true</c> if the <see cref="DebugString"/> has been removed within the specified time;
+        /// otherwise, <c>false</c>.
+        /// </returns>
+        [SecuritySafeCritical]
+        public bool TryTake(out DebugString value, TimeSpan timeout, CancellationToken cancellationToken)
+        {
+            long timeoutMilliseconds = (long)timeout.TotalMilliseconds;
+
+            if ((timeoutMilliseconds < Timeout.Infinite) || (timeoutMilliseconds > int.MaxValue))
+            {
+                throw new ArgumentOutOfRangeException("timeout", "Timeout in milliseconds must be either non-negative and less than or equal to Int32.MaxValue or Timeout.Infinite (-1).");
+            }
+
+            return this.TryTake(out value, (int)timeoutMilliseconds, cancellationToken);
+        }
+
+        /// <summary>
+        /// Attempts to remove a <see cref="DebugString" /> from the buffer contained by this
+        /// <see cref="DebugView"/> within the specified time while observing a cancellation token.
+        /// </summary>
+        /// <param name="value">
+        /// When <see cref="TryTake(DebugString,int,CancellationToken)"/> returns, contains the removed
+        /// <see cref="DebugString"/>, if successful; otherwise, an empty <see cref="DebugString"/>.
+        /// </param>
+        /// <param name="timeout">
+        /// The number of milliseconds to wait, or <see cref="Timeout.Infinite"/> (-1) to wait
+        /// indefinitely.
+        /// </param>
+        /// <param name="cancellationToken">
+        /// The cancellation token to observe.
+        /// </param>
+        /// <returns>
+        /// <c>true</c> if the <see cref="DebugString"/> has been removed within the specified time;
+        /// otherwise, <c>false</c>.
+        /// </returns>
+        [SecuritySafeCritical]
+        public bool TryTake(out DebugString value, int timeout, CancellationToken cancellationToken)
+        {
+            if (!this.attached)
+            {
+                this.Wait(timeout, cancellationToken);
+
+                value = default(DebugString);
+                return false;
+            }
+
+            if (!this.TryWaitForDataReady(timeout, cancellationToken))
+            {
+                value = default(DebugString);
+                return false;
+            }
+
+            try
+            {
+                value = this.ReadDebugString();
+                return true;
+            }
+            finally
+            {
+                this.bufferReadyEvent.Set();
+            }
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting
+        /// unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            IDisposable d;
+            
+            d = this.dataBufferFile;
+
+            if (d != null)
+            {
+                this.dataBufferFile = null;
+                d.Dispose();
+            }
+
+            d = this.bufferReadyEvent;
+
+            if (d != null)
+            {
+                this.bufferReadyEvent = null;
+                d.Dispose();
+            }
+
+            d = this.dataReadyEvent;
+
+            if (d != null)
+            {
+                this.dataReadyEvent = null;
+                d.Dispose();
+            }
+
+            this.dataBuffer = null;
+        }
+
+        /// <summary>
+        /// Attempts to wait until data is ready within the specified time while observing
+        /// the cancellation token.
+        /// </summary>
+        /// <param name="timeout">
+        /// The number of milliseconds to wait, or <see cref="Timeout.Infinite"/> (-1) to wait
+        /// indefinitely.
+        /// </param>
+        /// <param name="cancellationToken">
+        /// The cancellation token to observe.
+        /// </param>
+        /// <returns>
+        /// <c>true</c> if data is ready; otherwise, <c>false</c>.
+        /// </returns>
+        private bool TryWaitForDataReady(int timeout, CancellationToken cancellationToken)
+        {
+            if (cancellationToken != CancellationToken.None)
+            {
+                cancellationToken.ThrowIfCancellationRequested();
+
+                int waitResult = WaitHandle.WaitAny(
+                    new[]
+                    {
+                        this.dataReadyEvent,
+                        cancellationToken.WaitHandle
+                    },
+                    timeout);
+
+                if (waitResult == 1)
+                {
+                    throw new OperationCanceledException(cancellationToken);
+                }
+
+                return waitResult == 0;
+            }
+
+            return this.dataReadyEvent.WaitOne(timeout);
+        }
+
+        /// <summary>
+        /// Waits within the specified time while observing the cancellation token.
+        /// </summary>
+        /// <param name="timeout">
+        /// The number of milliseconds to wait, or <see cref="Timeout.Infinite"/> (-1) to wait
+        /// indefinitely.
+        /// </param>
+        /// <param name="cancellationToken">
+        /// The cancellation token to observe.
+        /// </param>
+        private void Wait(int timeout, CancellationToken cancellationToken)
+        {
+            if (cancellationToken == CancellationToken.None)
+            {
+                Thread.Sleep(timeout);
+            }
+            else if (cancellationToken.WaitHandle.WaitOne(timeout))
+            {
+                throw new OperationCanceledException(cancellationToken);
+            }
+        }
+        
+        /// <summary>
+        /// Reads the <see cref="DebugString"/> from the underlying memory-mapped file.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="DebugString"/> read from the underlying memory-mapped file.
+        /// </returns>
+        [SecurityCritical]
+        private unsafe DebugString ReadDebugString()
+        {
+            using (var viewStream = this.dataBufferFile.CreateViewStream())
+            {
+                int bytesRead = viewStream.Read(this.dataBuffer, 0, this.dataBuffer.Length);
+                Debug.Assert(bytesRead == DataBufferLength, "bytesRead is not 4096");
+
+                // CORRECTNESS Skip the first 4 bytes in the buffer that indicate the process ID when searching for the null terminator.
+                int terminator = Array.IndexOf(this.dataBuffer, 0, sizeof(int));
+                Debug.Assert((terminator < 0) || (terminator >= sizeof(int)), "terminator is between 0 and 3");
+
+                if (terminator < 0)
+                {
+                    // ROBUSTNESS Null terminator not found, assume it is placed after the last byte in the buffer.
+                    terminator = DataBufferLength;
+                }
+
+                int processId = BitConverter.ToInt32(this.dataBuffer, 0);
+                string value = Encoding.Default.GetString(this.dataBuffer, sizeof(int), terminator - sizeof(int));
+
+                return new DebugString(processId, value);
+            }
+        }
+    }
+}
